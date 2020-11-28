d2020c524b1c171da6fcc91a4dd53f6083776f77
Tamás Kakuszi <tamas.kakuszi@gmail.com>
2020-11-28 20:14:13 +0100

    added payment

--- counter/settings.py
+++ counter/settings.py
@@ -79,10 +79,18 @@ WSGI_APPLICATION = 'counter.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    'new': {
+             'ENGINE': 'django.db.backends.sqlite3',
+             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproject',
+        'USER': 'root',
+        'PASSWORD': 'TEST1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    },
 }
 
 
@@ -134,3 +142,4 @@ DELIVERY_PRICE = 1735
 CSOMAGKULDO_PRICE = 460
 
 CURRENCY = 'huf'
+
