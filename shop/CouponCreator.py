5e830727ac49604407fcd9f708daacba2e6a5216
Tamás Kakuszi <tamas.kakuszi@gmail.com>
2021-03-08 20:43:09 +0100

    fix oupon generator to handle more than 100 products

--- shop/CouponCreator.py
+++ shop/CouponCreator.py
@@ -20,8 +20,13 @@ class CouponCreator:
             pass
 
         # get products
-        products = [p.get('id') for p in stripe.Product.list(active=True, limit=100).get('data') if
-                    p.get('id') not in self.delivery_products]
+        all_products = []
+        products = stripe.Product.list(active=True, limit=100)
+        all_products.extend([p.get('id') for p in products.get('data') if p.get('id') not in self.delivery_products])
+        while products.get('has_more'):
+            products = stripe.Product.list(active=True, limit=100, starting_after=products.get('data')[-1])
+            all_products.extend([p.get('id') for p in products.get('data') if p.get('id')
+                                 not in self.delivery_products])
 
         # create new coupon with list of products
         response = stripe.Coupon.create(
