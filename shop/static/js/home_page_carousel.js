db184652b889e8974cba3bd56d9aa46a5a7d8b1d
Tamás Kakuszi <tamas.kakuszi@gmail.com>
2021-12-04 20:04:08 +0100

    change order of init owl

--- shop/static/js/home_page_carousel.js
+++ shop/static/js/home_page_carousel.js
@@ -1,17 +1,6 @@
 $(document).ready(function () {
     var a = $("#owl-home-carousel");
     var b = $("#owl-products-carousel");
-    a.owlCarousel({
-        loop: !0,
-        margin: 30,
-        lazyLoad: !0,
-        nav: !1,
-        dots: !0,
-        autoplay: !0,
-        autoplayTimeout: 3e3,
-        autoplayHoverPause: !1,
-        animateOut: "fadeOut",
-        responsive: { 0: { items: 1 }, 600: { items: 1 }, 1025: { items: 1 } } }),
     b.owlCarousel({
         stagePadding: 50,
         loop: !0,
@@ -25,4 +14,15 @@ $(document).ready(function () {
         navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
         responsive: { 0: { items: 1 }, 600: { items: 2 }, 1025: { items: 4 } }
     });
+    a.owlCarousel({
+        loop: !0,
+        margin: 30,
+        lazyLoad: !0,
+        nav: !1,
+        dots: !0,
+        autoplay: !0,
+        autoplayTimeout: 3e3,
+        autoplayHoverPause: !1,
+        animateOut: "fadeOut",
+        responsive: { 0: { items: 1 }, 600: { items: 1 }, 1025: { items: 1 } } })
 });
