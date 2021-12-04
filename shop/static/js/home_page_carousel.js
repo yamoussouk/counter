914b6d95f442e1a352f00e5f671a3fff6ccaeaea
Tamás Kakuszi <tamas.kakuszi@gmail.com>
2021-12-04 19:52:20 +0100

    fix on js file

--- shop/static/js/home_page_carousel.js
+++ shop/static/js/home_page_carousel.js
@@ -9,7 +9,8 @@ $(document).ready(function () {
         dots: !0,
         autoplay: !0,
         autoplayTimeout: 3e3,
-        autoplayHoverPause: !1,  
+        autoplayHoverPause: !1,
+        animateOut: "fadeOut",
         responsive: { 0: { items: 1 }, 600: { items: 1 }, 1025: { items: 1 } } }),
     b.owlCarousel({
         stagePadding: 50,
@@ -22,6 +23,6 @@ $(document).ready(function () {
         autoplayTimeout: 3e3,
         autoplayHoverPause: !1,
         navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-        responsive: { 0: { items: 1 }, 600: { items: 2 }, 1025: { items: 4 } },
+        responsive: { 0: { items: 1 }, 600: { items: 2 }, 1025: { items: 4 } }),
     });
 });
