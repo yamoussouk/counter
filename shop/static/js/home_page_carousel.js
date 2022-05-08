703d3185443f6d41392427c4278498f9f31af109
Tamás Kakuszi <tamas.kakuszi@gmail.com>
2022-05-08 09:55:59 +0200

    added best sellers carousel to the home page

--- shop/static/js/home_page_carousel.js
+++ shop/static/js/home_page_carousel.js
@@ -1,6 +1,22 @@
 $(window).on('load',function() {
     var a = $("#owl-home-carousel");
     var b = $("#owl-products-carousel");
+    var c = $("#owl-bs-products-carousel");
+    if (($('#bs-carousel-row')).is(":visible")) {
+        c.owlCarousel({
+            stagePadding: 50,
+            loop: !0,
+            margin: 30,
+            lazyLoad: !0,
+            nav: !0,
+            dots: !1,
+            autoplay: !0,
+            autoplayTimeout: 3e3,
+            autoplayHoverPause: !1,
+            navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+            responsive: { 0: { items: 1 }, 600: { items: 2 }, 1025: { items: 4 } }
+        });
+    }
     b.owlCarousel({
         stagePadding: 50,
         loop: !0,
