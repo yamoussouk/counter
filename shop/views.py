68dfaed60c5dd717be20d7096d3472ffc1faf4e7
Tamás Kakuszi <tamas.kakuszi@gmail.com>
2023-02-09 19:13:00 +0100

    hotfix for errors

--- shop/views.py
+++ shop/views.py
@@ -32,7 +32,7 @@ log = logging.getLogger(__name__)
 
 def mobile(request):
     MOBILE_AGENT_RE = re.compile(r".*(iphone|mobile|androidtouch)", re.IGNORECASE)
-    if MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
+    if 'HTTP_USER_AGENT' in request.META and MOBILE_AGENT_RE.match(request.META['HTTP_USER_AGENT']):
         return True
     else:
         return False
